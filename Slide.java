6a8451072f5403568e887007f8b7f67c3c83de4a
Derek <derekyin27@gmail.com>
2021-02-22 13:08:09 -0500

    slide and slidelistnode

--- Slide.java
+++ Slide.java
@@ -1,3 +1,50 @@
 public class Slide{
+  public static final int MAX_BULLETS = 5;
+  private String title;
+  private double duration;
+
+  public Slide(){
+    duration = 0.0;
+  }
+
+  public Slide(String newTitle, double newDuration){
+    title = newTitle;
+    duration = newDuration;
+  }
+
+  public String getTitle(){
+    return title;
+  }
+
+  public void setTitle(String newTitle){
+    if (newTitle != null){
+      title = newTitle;
+    }
+    else throw new IllegalArgumentException();
+  }
+
+  public double getDuration(){
+    return duration;
+  }
+
+  public void setDuration(double newDuration){
+    if (newDuration > 0.0){
+      duration = newDuration;
+    }
+    else throw new IllegalArgumentException();
+  }
+
+  public int getNumBullets(){
+
+  }
+
+  public String getBullet(int i){
+
+  }
+
+  public void setBullet(String bullet, int i){
+
+  }
+
   
 }
---
+++ SlideListNode.java
@@ -0,0 +1,41 @@
+public class SlideListNode{
+  private Slide data;
+  private SlideListNode next;
+  private SlideListNode prev;
+
+  public SlideListNode(Slide initData){
+    if (initData != null){
+      data = initData;
+    }
+    else throw new IllegalArgumentException();
+  }
+
+  public Slide getData(){
+    return data;
+  }
+
+  public void setData(Slide newData){
+    if (newData != null)]{
+      data = newData;
+    }
+    else throw new IllegalArgumentException();
+  }
+
+  public SlideListNode getNext(){
+    return next;
+  }
+
+  public void setNext(SlideListNode newNext){
+    next = newNext;
+  }
+
+  public SlideListNode getPrev(){
+    return prev;
+  }
+
+  public void setPrev(SlideListNode newPrev){
+    prev = newPrev;
+  }
+
+
+}
