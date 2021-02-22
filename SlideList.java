14ec2a1c14854dae8e8a24edfc58c4e7720512df
Derek <derekyin27@gmail.com>
2021-02-22 13:15:44 -0500

    created all classes

---
+++ SlideList.java
@@ -0,0 +1,49 @@
+public class SlideList{
+  private SlideListNode head;
+  private SlideListNode tail;
+  private SlideListNode cursor;
+
+  public SlideList(){
+
+  }
+
+  public int size(){
+
+  }
+
+  public double duration(){
+
+  }
+
+  public int numBullets(){
+
+  }
+
+  public Slide getCursorSlide(){
+
+  }
+
+  public void resetCursorToHead(){
+    cursor = head;
+  }
+
+  public void cursorForward(){
+
+  }
+
+  public void cursorBackward(){
+
+  }
+
+  public void insertBeforeCursor(Slide newSlide){
+
+  }
+
+  public void appendToTail(Slide newSlide){
+
+  }
+
+  public slide removeCursor(){
+
+  }
+}
--- SlideListNode.java
+++ SlideListNode.java
@@ -37,5 +37,4 @@ public class SlideListNode{
     prev = newPrev;
   }
 
-
 }
